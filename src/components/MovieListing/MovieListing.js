95525cb4050001cfc12a907f5e307dda5ed82795
sean <seanschmidt89@yahoo.com>
2022-10-15 00:59:06 -0700

    added search functionality

--- src/components/MovieListing/MovieListing.js
+++ src/components/MovieListing/MovieListing.js
@@ -8,11 +8,22 @@ import "./MovieListing.css";
 
 const MovieListing = () => {
   const [inputText, setInputText] = useState("");
+  const [search, setSearch] = useState("star wars");
   const dispatch = useDispatch();
   const movies = useSelector((state) => state.movies.movies);
 
+  const getMovies = async () => {
+    const response = await baseUrl
+      .get(`?apikey=${apiKey}&type=movie&s=${search}`)
+      .catch((error) => {
+        console.log("Error message:", error);
+      });
+    if (response.request.status === 200) {
+      dispatch(movieSliceActions.addMovies(response.data));
+    }
+  };
+
   useEffect(() => {
-    const search = "star wars";
     const getMovies = async () => {
       const response = await baseUrl
         .get(`?apikey=${apiKey}&type=movie&s=${search}`)
@@ -24,8 +35,7 @@ const MovieListing = () => {
       }
     };
     getMovies();
-  }, [dispatch]);
-  console.log(movies);
+  }, [dispatch, search, setSearch]);
 
   const inputHandler = (e) => {
     setInputText(e.target.value);
@@ -33,22 +43,34 @@ const MovieListing = () => {
 
   const searchMoviesHandler = (e) => {
     e.preventDefault();
+    setSearch(inputText);
+    getMovies();
     setInputText("");
   };
   return (
     <div>
       <div className="search">
         <form onSubmit={searchMoviesHandler}>
-          <input onChange={inputHandler} value={inputText} placeholder="Search Moives" />
-          <button type="submit" className="search-btn">Search</button>
+          <input
+            onChange={inputHandler}
+            value={inputText}
+            placeholder="Search Moives"
+          />
+          <button type="submit" className="search-btn">
+            Search
+          </button>
         </form>
       </div>
-      <div className="container">
-        {movies.Search &&
-          movies.Search.map((movie, index) => (
+
+      {movies.Search ? (
+        <div className="container">
+          {movies.Search.map((movie, index) => (
             <MovieCard key={index} movie={movie} />
           ))}
-      </div>
+        </div>
+      ) : (
+        <p className="error">Cound'nt find anything with that title...</p>
+      )}
     </div>
   );
 };
